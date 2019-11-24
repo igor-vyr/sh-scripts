9d242a5c3f470683477bc8b065c40fe170a5bce3
igor-vyr <ikossaczky@gmail.com>
2019-11-24 14:25:19 +0100

    init commit

---
+++ pyscript.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python3
+print("welcome, it works, i'm printing from python!")
+x=[1,2, "a"]
+print("some list", x)
---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Author : Zara Ali
+# Copyright (c) Tutorialspoint.com
+# Script follows here:
+
+echo "What is your name?"
+read PERSON
+echo "Hello, $PERSON"
+cd ..
+ls
+git status
+date
+echo "ok...." 
